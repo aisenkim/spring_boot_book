013f05c9eeff138ab5ea50f00219f34da78514f4
aisen <aisenkim15@gmail.com>
2021-07-23 15:07:28 +0900

    before update to junit 5

--- src/main/java/com/aisen/book/spring_boot_book/service/posts/PostsService.java
+++ src/main/java/com/aisen/book/spring_boot_book/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.aisen.book.spring_boot_book.service.posts;
 
+import com.aisen.book.spring_boot_book.domain.posts.Posts;
 import com.aisen.book.spring_boot_book.domain.posts.PostsRepository;
+import com.aisen.book.spring_boot_book.web.dto.PostsResponseDto;
 import com.aisen.book.spring_boot_book.web.dto.PostsSaveRequestDto;
+import com.aisen.book.spring_boot_book.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +19,23 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("No posts by the provided id: " + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("No posts by the provided id: " + id));
+
+        // returning dto because don't need everything from the entity
+        return new PostsResponseDto(entity);
     }
 }
