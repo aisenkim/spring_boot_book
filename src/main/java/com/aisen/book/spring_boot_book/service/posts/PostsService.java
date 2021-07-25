bda4b4642e415085991ef9186655dbc3f3c4fd45
aisen <aisenkim15@gmail.com>
2021-07-26 01:40:44 +0900

    finished screen / template part

--- src/main/java/com/aisen/book/spring_boot_book/service/posts/PostsService.java
+++ src/main/java/com/aisen/book/spring_boot_book/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.aisen.book.spring_boot_book.service.posts;
 
 import com.aisen.book.spring_boot_book.domain.posts.Posts;
 import com.aisen.book.spring_boot_book.domain.posts.PostsRepository;
+import com.aisen.book.spring_boot_book.web.dto.PostsListResponseDto;
 import com.aisen.book.spring_boot_book.web.dto.PostsResponseDto;
 import com.aisen.book.spring_boot_book.web.dto.PostsSaveRequestDto;
 import com.aisen.book.spring_boot_book.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -38,5 +41,18 @@ public class PostsService {
 
         // returning dto because don't need everything from the entity
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("The post doesn't exist: " + id));
+        postsRepository.delete(posts);
     }
 }
