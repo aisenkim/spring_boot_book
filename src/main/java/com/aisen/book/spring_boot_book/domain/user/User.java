990d902d4dca414bb6ca65701d23764e09e7f7b0
Aisen Kim <aisenkim15@gmail.com>
2021-08-04 23:06:34 +0900

    update

--- src/main/java/com/aisen/book/spring_boot_book/domain/user/User.java
+++ src/main/java/com/aisen/book/spring_boot_book/domain/user/User.java
@@ -10,6 +10,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
+@Table(name = "user", schema = "public")
 public class User extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
