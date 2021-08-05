eb796a71799f5989b7a8f2642004929975309105
Aisen Kim <aisenkim15@gmail.com>
2021-08-05 20:17:25 +0900

    added API to determine if use 8081 or 8082

---
+++ src/test/java/com/aisen/book/spring_boot_book/web/ProfileControllerTest.java
@@ -0,0 +1,34 @@
+package com.aisen.book.spring_boot_book.web;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ProfileControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void profile_called_without_authentication() {
+        String expected = "default";
+
+        ResponseEntity<String> response = restTemplate.getForEntity("/profile", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isEqualTo(expected);
+    }
+
+}
---
+++ src/test/java/com/aisen/book/spring_boot_book/web/ProfileControllerUnitTest.java
@@ -0,0 +1,60 @@
+package com.aisen.book.spring_boot_book.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.env.MockEnvironment;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class ProfileControllerUnitTest {
+
+    @Test
+    public void real_profile_check() {
+        // given
+        String expectedProfile = "real";
+        MockEnvironment env = new MockEnvironment();
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("oauth");
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        // when
+        String profile = controller.profile();
+
+        // then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void get_first_if_no_real_profile() {
+        // given
+        String expectedProfile = "oauth";
+        MockEnvironment env = new MockEnvironment();
+
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("read-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        // when
+        String profile = controller.profile();
+
+        // then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void get_default_if_no_default() {
+        // given
+        String expectedProfile = "default";
+        MockEnvironment env = new MockEnvironment();
+        ProfileController controller = new ProfileController(env);
+
+        // when
+        String profile = controller.profile();
+
+        // then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+}
